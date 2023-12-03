935f35f291319ad07937c3a7a444d777e117a8f5
Talmadge Farringer <tjfarrin@gmail.com>
2023-12-03 10:20:34 -0500

    first pass at day 3

---
+++ day_3.py
@@ -0,0 +1,50 @@
+import utilities
+
+
+def day_three(path):
+    part_numbers = []
+    # Read the entire txt file into memory
+    with open(path) as f:
+        data = f.readlines()
+
+    data = ["48.................501....33.....622..............763.........331.................161.683......................................980..........",
+            "...491.842.....948*..................338.....*......=...........-...309.......633*....*....................*990...706...452......*..+......."]
+
+    for line_idx, line in enumerate(data):
+        first_number_indx_rng = 0
+        search_range = 0
+        while not first_number_indx_rng is None:
+            # Steps:
+            # 1 -- check line for first number
+            print(f'test serach rng: {search_range}. {len(data[0])}')
+            first_number_indx_rng = utilities.find_first_int(line[search_range: len(data[0])], reversed=False, letters_as_words=False,
+                                                             find_entire_number=True, indices_rather_than_number=True)
+            print(f'checking: {first_number_indx_rng}')
+            # If no more numbers then move onto the next line
+            if first_number_indx_rng is None:
+                continue
+            # 2 -- check if that number is surrounded by a symbol
+            surrounded = utilities.surrounded_by_symbol(
+                data, line_idx, first_number_indx_rng)
+            # 3 -- if so, store, otherwise move onto the next number
+            if surrounded:
+                print(
+                    f'value: {data[line_idx][first_number_indx_rng[0]:first_number_indx_rng[1]]}')
+                part_numbers.append(
+                    int(data[line_idx][first_number_indx_rng[0]:first_number_indx_rng[1]]))
+
+            search_range = first_number_indx_rng[1]+1
+
+    return sum(part_numbers)
+
+
+# print(day_three("day_3_input.txt"))
+# tuple_returned = utilities.find_first_int("Hel123lo", reversed=False, letters_as_words=False,
+#                                           find_entire_number=True, indices_rather_than_number=True)
+
+# print(tuple_returned[0])
+# print(tuple_returned[1])
+# find_first_int(line, reversed=False, letters_as_words=False, find_entire_number=False, indices_rather_than_number=False):
+
+data = "48.................501....33.....622..............763.........331.................161.683......................................980.........."
+print(data[17:20])
--- utilities.py
+++ utilities.py
@@ -8,19 +8,24 @@ def read_txt_file(path):
     return file.readlines()
 
 
-def find_first_int(line, reversed=False, letters_as_words=False, find_entire_number=False):
+def find_first_int(line, reversed=False, letters_as_words=False, find_entire_number=False, indices_rather_than_number=False):
     for indx, char in enumerate(line):
         # If the char is a number then return it
         if char.isdigit():
             # One problem wants the entire digit rather than just the first int
             # This path captures that
             if find_entire_number:
+                start_idx = indx
                 number = ""
                 number += str(char)
                 for x in range(indx+1, len(line)):
                     if line[x].isdigit():
                         number += str(line[x])
                     else:
+                        if indices_rather_than_number:
+                            end_indx = x - 1
+                            return (start_idx, end_indx)
+
                         return int(number)
             # This path gives us the first int
             else:
@@ -36,3 +41,43 @@ def find_first_int(line, reversed=False, letters_as_words=False, find_entire_num
                 if char == key[0]:
                     if line[indx:len(key)+indx] == key:
                         return int_dict[orig_key]
+
+
+def is_symbol(value):
+    special_characters = '"!@#$%^&*()-+?_=,<>/"'
+    if any(c in special_characters for c in value):
+        return True
+    return False
+
+
+def check_neighbors(data, line_number, entry_nbr):
+    if line_number - 1 >= 0:
+        # todo add boundary checks here
+        for x in range(entry_nbr-1, entry_nbr+1):
+            if x >= 0 and x < len(data[0]):
+                if is_symbol(data[line_number-1][x]):
+                    return True
+    if entry_nbr-1 >= 0:
+        if is_symbol(data[line_number][entry_nbr-1]):
+            return True
+    if entry_nbr+1 < len(data[0]):
+        if is_symbol(data[line_number][entry_nbr+1]):
+            return True
+    if line_number + 1 < len(data[0]):
+        for x in range(entry_nbr-1, entry_nbr+1):
+            if x >= 0 and x < len(data[0]):
+                if is_symbol(data[line_number+1][x]):
+                    return True
+
+    return False
+
+
+def surrounded_by_symbol(data, line_number, entry_rng):
+    # for each number in the range
+    # does it's immediate neighbors include a symbol?
+    # If so, return True
+    for entry in range(entry_rng[0], entry_rng[1]+1):
+        if check_neighbors(data, line_number, entry):
+            return True
+
+    return False
