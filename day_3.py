6100385d473dd84584edbc22e0ec88b5d84c5add
Talmadge Farringer <tjfarrin@gmail.com>
2024-01-23 10:51:39 -0500

    remove irr comment

--- day_3.py
+++ day_3.py
@@ -1,14 +1,30 @@
+from collections import defaultdict
+import sys
+import math as m
+import re
 import utilities
 
 
 def day_three(path):
+    gears = defaultdict(list)
     part_numbers = []
     # Read the entire txt file into memory
     with open(path) as f:
         data = f.readlines()
 
-    data = ["48.................501....33.....622..............763.........331.................161.683......................................980..........",
-            "...491.842.....948*..................338.....*......=...........-...309.......633*....*....................*990...706...452......*..+......."]
+    # data = ["467..114..",
+    #         "/..*......",
+    #         "..35..633.",
+    #         "......#...",
+    #         "617*......",
+    #         ".....+.58.",
+    #         "..592.....",
+    #         "......755.",
+    #         "...$.*.*..",
+    #         ".664.598./"]
+
+    # data = ["349...*.............402...735......@................182.121...134........%255.276...%...&.../............460....#......79...................",
+    #         "....503....22.............#.....$............38.....+...........*....-11.......*......326...../....*853.........216......*.../180.....16...."]
 
     for line_idx, line in enumerate(data):
         first_number_indx_rng = 0
@@ -26,15 +42,13 @@ def day_three(path):
 
             # Need to update the number-indx-range
             # because that fncn is retruning values as of the edited line
-            # first_number_indx_rng[0] = first_number_indx_rng[0] + search_range
-            # first_number_indx_rng[1] += search_range
-
+            # adding 1 to the end range index because we want to search upto and including that last digit
             updated_indx = (
                 first_number_indx_rng[0]+search_range, first_number_indx_rng[1]+search_range+1)
 
             # 2 -- check if that number is surrounded by a symbol
             surrounded = utilities.surrounded_by_symbol(
-                data, line_idx, updated_indx)
+                data, line_idx, updated_indx, gears)
             # 3 -- if so, store, otherwise move onto the next number
             if surrounded:
                 # print(
@@ -44,20 +58,17 @@ def day_three(path):
 
             search_range = updated_indx[1]+1
 
-    print(part_numbers)
+    # print(part_numbers)
+    # Removing duplicates did not give the correct results..
+    # part_numbers = list(set(part_numbers))
     return sum(part_numbers)
 
 
-print(day_three("day_3_input.txt"))
-# tuple_returned = utilities.find_first_int("Hel123lo", reversed=False, letters_as_words=False,
-#                                           find_entire_number=True, indices_rather_than_number=True)
+# print(day_three("day_3_input.txt"))
+# 522985 --
 
-# print(tuple_returned)
-# print(tuple_returned[1])
-# find_first_int(line, reversed=False, letters_as_words=False, find_entire_number=False, indices_rather_than_number=False):
 
-# data = ["48.................501....33.....622..............763.........331.................161.683......................................980..........",
-#         "...491.842.....948*..................338.....*......=...........-...309.......633*....*....................*990...706...452......*..+......."]
-
-# # print(data[17:20])
-# print(len(data[0]))
+gears = defaultdict(list)
+gears[(3, 2)].append((2, 3))
+gears[(3, 2)].append((5, 8))
+print(gears)
